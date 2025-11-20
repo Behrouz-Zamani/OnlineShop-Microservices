dc95ddd57ddaef03a871a04c02fdacc39da04dbd
Behrouz Zamani <zamani.behrouz@hotmail.com>
2025-11-20 02:47:24 -0600

    change auth

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a144e676c1992760262e1c03d8f8153ae6a0f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8e621f56d2ecb5d303dc38721d709e8dc8f656")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
